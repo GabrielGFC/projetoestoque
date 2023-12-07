71c464fdcaf5441aa5295585141b95cad05253a9
amabilee <dev.amabile@proton.me>
2023-12-07 14:17:44 -0300

    Terceiro commit

--- Back-end/src/models/userModel.js
+++
@@ -1,9 +0,0 @@
-class User {
-  constructor(matricula, senha, tipo) {
-    this.matricula = matricula;
-    this.senha = senha;
-    this.tipo = tipo; // Aluno ou Colaborador
-  }
-}
-
-module.exports = User;
